7a63c5018d563625aca5e7581073135d3105211b
bc-andreadao <andrea.dao@bigcommerce.com>
2025-07-07 13:25:39 -0700

    integrate with apollo client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { DisplayLocations } from './DisplayLocations';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <DisplayLocations />
     </div>
   );
 }
-
-export default App;
