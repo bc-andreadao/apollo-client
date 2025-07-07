e5450a0ca49e2fa2450eda85d90283c44b942270
bc-andreadao <andrea.dao@bigcommerce.com>
2025-07-07 15:31:01 -0700

    add authorization header for remote graphql schema

--- codegen.ts
+++ codegen.ts
@@ -1,9 +1,19 @@
-
+import dotenv from 'dotenv';
 import type { CodegenConfig } from '@graphql-codegen/cli';
+
+dotenv.config({ path: '.env.local' });
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://store-o8ertuc7vw-1730162.mybigcommerce.com/graphql",
+  schema: [
+    {
+      "https://store-o8ertuc7vw-1730162.mybigcommerce.com/graphql": {
+        headers: {
+          Authorization: `Bearer ${process.env.BIGCOMMERCE_API_TOKEN}`,
+        },
+      },
+    },
+  ],
   documents: "./src/**/*.graphql",
   generates: {
     "./src/graphql/generated.ts": {
