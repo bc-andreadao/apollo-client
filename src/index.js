32ec8d99b7973b1bc912c5e1bbed0ee31df46f9b
bc-andreadao <andrea.dao@bigcommerce.com>
2025-07-08 11:17:40 -0700

    update client URI

--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ import reportWebVitals from './reportWebVitals';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://flyby-router-demo.herokuapp.com/',
+  uri: 'https://store-o8ertuc7vw-1730162.mybigcommerce.com/graphql',
   cache: new InMemoryCache(),
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_BIGCOMMERCE_API_TOKEN}`,
+  },
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
